fe2453bc556b1c411cafc0a6562d7685f09325d2
Arbab Husain <arbabhusain200@gmail.com>
2023-12-20 23:23:19 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let coordinateSX;
-let coordinateSY; 
-let coordinateNX; 
-let coordinateNY; 
+
+
+
 
 /*
 function updateCoordinatesAndRedraw() {
@@ -180,6 +179,8 @@ connectNodes.closePath();
 
 }
 */
+
+
 let map = document.getElementById("map");
 
 
@@ -201,24 +202,54 @@ let node6 = map.getContext("2d");
 
 var colorFill = "rgb(73, 16, 139)";
 var colorEmpty = "rgb(73, 16, 139, 0.2)";
-var NX = 40.8231;
-var NY = coordinateNY;
-var SX = coordinateSX;
-var SY = coordinateSY;
+
+/*
+let coordinateSX;
+let coordinateSY; 
+let coordinateNX; 
+let coordinateNY;
+
+let NX = coordinateNX;
+let NY = coordinateNY;
+let SX = coordinateSX;
+let SY = coordinateSY;
+*/
+let context = map.getContext("2d");
+
+function clearCanvas() {
+    context.clearRect(0, 0, map.width, map.height);
+}
+
+let randomNumber = 0;
+let randomNumber2 = 3;
+
+// Get the element by its ID
+let Stime = document.getElementById("Stime");
+let Ntime = document.getElementById("Ntime");
+let CStime = document.getElementById("CStime");
+let CNtime = document.getElementById("CNtime");
+
+
+function Draw(cnt, cnt2){
+
+clearCanvas();
 
 //node 1(Marshak)
 node.beginPath();
 node.strokeStyle = "#172e51";
-if (NX == 5) {
-  node.fillStyle = colorFill;
-}
-else{
-  node.fillStyle = colorEmpty;
-}
 node.lineWidth = 3;
 node.arc(400, 300, 10, 0, Math.PI * 2, false);
 node.stroke();
-node.fill();
+if (cnt == 0 || cnt2 == 0) {
+  node.fillStyle = colorFill;
+  node.fill();
+  Stime.textContent = "7";
+  Ntime.textContent = "7";
+}
+else{
+  node.fillStyle = colorEmpty;
+  node.fill();
+}
 node.closePath();
 
 //node 2(NAC)
@@ -228,9 +259,14 @@ node2.strokeStyle = "#172e51";
 node2.lineWidth = 3;
 node2.arc(400, 200, 10, 0, Math.PI * 2, false);
 node2.stroke();
-if (SX > 40.81700 || NX < 40.8200) {
+//SX > 40.81700 || NX < 40.8200
+if (cnt == 3 || cnt2 == 3) {
   node2.fillStyle = colorFill;
   node2.fill();
+  Stime.textContent = "7";
+  Ntime.textContent = "7";
+  CStime.textContent = "0";
+  CNtime.textContent = "0";
 }
 else{
   node2.fillStyle = colorEmpty;
@@ -238,15 +274,18 @@ else{
 }
 node2.closePath();
 
-//node 3(125)
+//node 3(145 corner)
 node3.beginPath();
 node3.strokeStyle = "#172e51";
 node3.lineWidth = 3;
-node3.arc(700, 300, 10, 0, Math.PI * 2, false);
+node3.arc(700, 300, 7.5, 0, Math.PI * 2, false);
 node3.stroke();
-if (SY > -73.95200) {
+//SY > -73.95200
+if (cnt == 1 || cnt2 == 1) {
   node3.fillStyle = colorFill;
   node3.fill();
+  Ntime.textContent = "2";
+  CNtime.textContent = "9";
 }
 else{
   node3.fillStyle = colorEmpty;
@@ -254,15 +293,17 @@ else{
 }
 node3.closePath();
 
-//node 4(145)
+//node 4(124 corner)
 node4.beginPath();
 node4.strokeStyle = "#172e51";
 node4.lineWidth = 3;
-node4.arc(50, 300, 10, 0, Math.PI * 2, false);
+node4.arc(50, 300, 7.5, 0, Math.PI * 2, false);
 node4.stroke();
-if (NY > -73.95200) {
+if (cnt == 4 || cnt2 == 4) {
   node4.fillStyle = colorFill;
   node4.fill();
+  Stime.textContent = "3";
+  CStime.textContent = "10";
 }
 else{
   node4.fillStyle = colorEmpty;
@@ -276,9 +317,11 @@ node5.strokeStyle = "#172e51";
 node5.lineWidth = 3;
 node5.arc(75, 500, 10, 0, Math.PI * 2, false);
 node5.stroke();
-if (SX < 40.8170) {
+if (cnt == 5 || cnt2 == 5) {
   node5.fillStyle = colorFill;
   node5.fill();
+  Stime.textContent = "0";
+  CStime.textContent = "7";
 }
 else{
   node5.fillStyle = colorEmpty;
@@ -292,9 +335,12 @@ node6.strokeStyle = "#172e51";
 node6.lineWidth = 3;
 node6.arc(650, 500, 10, 0, Math.PI * 2, false);
 node6.stroke();
-if (NX > 40.8200) {
+//NX > 40.8200
+if (cnt == 2 || cnt2 == 2) {
   node6.fillStyle = colorFill;
   node6.fill();
+  Ntime.textContent = "0";
+  CNtime.textContent = "7";
 }
 else{
   node6.fillStyle = colorEmpty;
@@ -356,10 +402,172 @@ text.fillText("SHUTTLE MAP", 325, 50);
 let text2 = map.getContext("2d");
 text.fillStyle = "red";
 text.font = "30px Trebuchet MS";
-text.fillText("LIVE", 255, 50);
+text.fillText("LIVE", 262, 50);
+
+let liveDot = map.getContext("2d");
+liveDot.beginPath();
+liveDot.strokeStyle = "#172e51";
+liveDot.lineWidth = 3;
+liveDot.arc(250, 40, 5, 0, Math.PI * 2, false);
+liveDot.fillStyle = "red";
+liveDot.stroke();
+liveDot.fill();
+liveDot.closePath();
+
+
+let NACLabel = map.getContext("2d");
+NACLabel.fillStyle = "#49108B";
+NACLabel.font = "20px Trebuchet MS";
+NACLabel.fillText("North Academic Center", 295, 175);
+
+let MARLabel = map.getContext("2d");
+MARLabel.fillStyle = "#49108B";
+MARLabel.font = "20px Trebuchet MS";
+MARLabel.fillText("Marshak", 355, 335);
+
+let SouthLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "20px Trebuchet MS";
+SouthLabel.fillText("125th Station", 50, 535);
+
+let SouthCornerLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "15px Trebuchet MS";
+SouthLabel.fillText("124th St Corner", 30, 270);
+
+let NorthLabel = map.getContext("2d");
+NorthLabel.fillStyle = "#49108B";
+NorthLabel.font = "20px Trebuchet MS";
+NorthLabel.fillText("145th Station", 600, 535);
+
+let NorthCornerLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "15px Trebuchet MS";
+SouthLabel.fillText("145th St Corner", 625, 270);
+
+
+
+
+}
+
+setInterval(function(){
+  Draw(randomNumber, randomNumber2);
+  randomNumber = (randomNumber + 1) % 6;
+  randomNumber2 = (randomNumber2 + 1) % 6;
+}, 7000);
+
+
+
+
+let text = map.getContext("2d");
+text.fillStyle = "#49108B";
+text.font = "30px Trebuchet MS";
+text.fillText("SHUTTLE MAP", 325, 50);
+
+let text2 = map.getContext("2d");
+text.fillStyle = "red";
+text.font = "30px Trebuchet MS";
+text.fillText("LIVE", 262, 50);
+
+let liveDot = map.getContext("2d");
+liveDot.beginPath();
+liveDot.strokeStyle = "#172e51";
+liveDot.lineWidth = 3;
+liveDot.arc(250, 40, 5, 0, Math.PI * 2, false);
+liveDot.fillStyle = "red";
+liveDot.stroke();
+liveDot.fill();
+liveDot.closePath();
+
+
+let NACLabel = map.getContext("2d");
+NACLabel.fillStyle = "#49108B";
+NACLabel.font = "20px Trebuchet MS";
+NACLabel.fillText("North Academic Center", 295, 175);
+
+let MARLabel = map.getContext("2d");
+MARLabel.fillStyle = "#49108B";
+MARLabel.font = "20px Trebuchet MS";
+MARLabel.fillText("Marshak", 355, 335);
+
+let SouthLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "20px Trebuchet MS";
+SouthLabel.fillText("125th Station", 50, 535);
+
+let SouthCornerLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "15px Trebuchet MS";
+SouthLabel.fillText("124th St Corner", 30, 270);
+
+let NorthLabel = map.getContext("2d");
+NorthLabel.fillStyle = "#49108B";
+NorthLabel.font = "20px Trebuchet MS";
+NorthLabel.fillText("145th Station", 600, 535);
+
+let NorthCornerLabel = map.getContext("2d");
+SouthLabel.fillStyle = "#49108B";
+SouthLabel.font = "15px Trebuchet MS";
+SouthLabel.fillText("145th St Corner", 625, 270);
+
+node.beginPath();
+node.strokeStyle = "#172e51";
+node.lineWidth = 3;
+node.arc(400, 300, 10, 0, Math.PI * 2, false);
+node.stroke();
+node.fillStyle = colorEmpty;
+node.fill();
+node.closePath();
+
+node2.beginPath();
+node2.strokeStyle = "#172e51";
+node2.lineWidth = 3;
+node2.arc(400, 200, 10, 0, Math.PI * 2, false);
+node2.stroke();
+node2.fillStyle = colorEmpty;
+node2.fill();
+node2.closePath();
+
+node3.beginPath();
+node3.strokeStyle = "#172e51";
+node3.lineWidth = 3;
+node3.arc(700, 300, 10, 0, Math.PI * 2, false);
+node3.stroke();
+node3.fillStyle = colorEmpty;
+node3.fill();
+node3.closePath();
+
+node4.beginPath();
+node4.strokeStyle = "#172e51";
+node4.lineWidth = 3;
+node4.arc(50, 300, 10, 0, Math.PI * 2, false);
+node4.stroke();
+node4.fillStyle = colorEmpty;
+node4.fill();
+node4.closePath();
+
+node5.beginPath();
+node5.strokeStyle = "#172e51";
+node5.lineWidth = 3;
+node5.arc(75, 500, 10, 0, Math.PI * 2, false);
+node5.stroke();
+node5.fillStyle = colorEmpty;
+node5.fill();
+node5.closePath();
+
+node6.beginPath();
+node6.strokeStyle = "#172e51";
+node6.lineWidth = 3;
+node6.arc(650, 500, 10, 0, Math.PI * 2, false);
+node6.stroke();
+node6.fillStyle = colorEmpty;
+node6.fill();
+node6.closePath();
+
+
 
 /*
-red flickery LIVE
+
 change coordinates to fill color of node
 */
 /*setTimeout(function () { 
